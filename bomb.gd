29ca2fa3c88f8a8e8c720e12b5ff4338b810166e
Harry Huang <sam.harry.huang@gmail.com>
2019-04-09 20:53:28 -0700

    Detach bomb from player

--- bomb.gd
+++ bomb.gd
@@ -8,7 +8,7 @@ func set_position(position):
 func _physics_process(delta):
 	var bomb_pressed = Input.is_action_pressed("bomb")
 	if bomb_pressed:
-		set_position(get_parent().position)
+		set_position(Vector2(20, 300))
 		
 func _integrate_forces(state):	
 	if new_position:
